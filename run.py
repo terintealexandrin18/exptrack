fc9831d7c9ba7bcf8d4061a3f0c6fb365eb69506
terintealexandrin18 <>
2023-11-09 16:19:26 +0000

    add funcion to see the remaining days left from the month for the daily budget

--- run.py
+++ run.py
@@ -26,26 +26,14 @@ class Expense:
         self.amount = amount
 
     def __repr__(self):
-        """
-        Convert the result to a string instead of printing the memory address
-        """
         return f"Expense: {self.name}, {self.category}, £{self.amount:.2f}"
 
 
 def remove_commas(input_str):
-    # Remove any commas from the input
     return input_str.replace(',', '')
 
 
 def get_user_expenses():
-    """
-    We use this function to collect data from the user, including the expense
-    name, category, and amount. If the user selects an incorrect category or
-    fails to enter a valid number for the amount, we print a ValueError. We use
-    the While True function to keep repeating the code until the user
-    enters the correct values.
-    """
-
     print(f" 🖍 Getting user expenses")
     name_of_expense = input("Enter your expense: ")
     while True:
@@ -54,7 +42,6 @@ def get_user_expenses():
 
         try:
             amount_of_expense = float(amount_of_expense)
-            # Input is successfully converted to a float, so break out of the loop
             break
         except ValueError:
             print("Invalid input. Please enter a valid number (e.g. 1200.50, 5, 7.23).")
@@ -87,12 +74,7 @@ def get_user_expenses():
 
 
 def save_expenses_to_google_sheet(expense_store):
-    """
-     Add the expense to a Google Sheet.
-    """
     print(f" 📌 Saving User {expense_store}")
-
-    # Convert the Expense object to a dictionary
     expense_dict = {
         "name": expense_store.name,
         "category": expense_store.category,
@@ -100,23 +82,16 @@ def save_expenses_to_google_sheet(expense_store):
     }
 
     page1_worksheet = SHEET.worksheet("page1")
-
-    # Append the dictionary to the Google Sheet
     page1_worksheet.append_row([expense_dict["name"],
                                 expense_dict["category"], expense_dict["amount"]])
     print("Saved successfully.🤗")
 
 
 def calculate_total_expenses_by_category():
-    """
-    Calculate the total expenses by category
-    Get the data from google sheet
-    """
     print("Calculating total expenses by category:")
     page1_worksheet = SHEET.worksheet("page1")
     data = page1_worksheet.get_all_values()
 
-    # Create a dictionary to categorize and calculate expenses by category
     categories_total = {}
     for row in data[1:]:
         name, category, amount = row
@@ -124,22 +99,15 @@ def calculate_total_expenses_by_category():
             categories_total[category] = 0
         categories_total[category] += float(amount)
 
-    # Print total expenses by category
     for category, total_amount in categories_total.items():
         print(f"{category}: £{total_amount:.2f}")
 
+
 def view_expenses_by_category():
-    """
-    View the total expenses by category
-    Get the date from google sheet. Use the while true
-    function to see all the expenses. Once done you can 
-    exit from the loop
-    """
     print("View expenses by category:")
     page1_worksheet = SHEET.worksheet("page1")
     data = page1_worksheet.get_all_values()
 
-    # Create a dictionary to categorize expenses
     categories = {}
     for row in data[1:]:
         name, category, amount = row
@@ -147,11 +115,9 @@ def view_expenses_by_category():
             categories[category] = []
         categories[category].append((name, float(amount)))
 
-    # Display the list of categories with corresponding numbers
     for index, category in enumerate(categories.keys(), start=1):
         print(f"{index}: {category}")
 
-    # Allow the user to choose a category to view expenses
     while True:
         category_choice = input("Enter the category number (0 to exit): ")
         if category_choice == "0":
@@ -170,9 +136,6 @@ def view_expenses_by_category():
 
 
 def total_amount_spent():
-    """
-    Function to see the total amount spent for all expenses and budget left
-    """
     print("View total amount spent this month:")
     page1_worksheet = SHEET.worksheet("page1")
     data = page1_worksheet.get_all_values()
@@ -186,30 +149,45 @@ def total_amount_spent():
 
 
 def set_up_monthly_budget():
-    """ 
-    Function to set up a monthly budget by the user using input method
-    """
-    global monthly_budget
+    global monthly_budget, total_spent
     try:
         monthly_budget = float(input("Enter your monthly budget: £"))
+
+        # Calculate total spent when setting up the monthly budget
+        total_spent = calculate_total_spent()
+
         return monthly_budget
     except ValueError:
         print("Invalid input. Please enter a valid numeric value.")
         return set_up_monthly_budget()
 
 
+def calculate_total_spent():
+    global total_spent
+    page1_worksheet = SHEET.worksheet("page1")
+    data = page1_worksheet.get_all_values()
+    total_spent = 0
+    for row in data[2:]:
+        amount = float(row[2])
+        total_spent += amount
+    return total_spent
+
+
 def monthly_budget_left():
-    """
-    Calculate the monthly budget left
-    """
     if monthly_budget == 0:
         print("Please set up a monthly budget to view the budget left.")
     else:
         budget_left = monthly_budget - total_spent
-        print(f"Monthly budget left: £{budget_left:.2f}")
 
+        now = datetime.datetime.now()
+        days_in_the_month = calendar.monthrange(now.year, now.month)[1]
+        remaining_days = days_in_the_month - now.day
+        daily_budget = budget_left / remaining_days
+        print(f"Monthly budget left: £{budget_left:.2f}, Budget per day left: £{daily_budget:.2f}")
 
+    
 def expenses_tracker_main():
+    calculate_total_spent()
     print("Welcome to Expense Tracker\n")
     while True:
         choice = input("Choose an option:\n  1: Enter an expense\n  2: View expenses by category\n  3: Calculate total expenses by category\n  4: Total amount spent this month\n  5: Set up monthly budget\n  6: View the monthly budget left\n  7: Exit")
